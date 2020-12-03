2b73a88c3dbbdb6b318befba1a2c87e1548d7eeb
Chan Chun Fatt <cfchan17@gmail.com>
2020-12-03 13:54:06 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,64 @@
+//Load libraries
+const express = require('express')
+const fortuneCookie = require('fortune-cookie')
+const morgan = require('morgan')
+const cors = require('cors')
+
+//Configure Port
+const PORT = parseInt(process.argv[2]) || parseInt(process.env.PORT) || 3000
+
+//Create an instance of express
+const app = express()
+
+//Function to get cookies
+function getCookies(count=1) {
+    if(count == 1) {
+        return {'cookie': fortuneCookie[Math.floor(Math.random() * fortuneCookie.length)]}
+    }
+    else {
+        let cookieCollection = []
+        let randNumArray = []
+        while(cookieCollection.length < count) {
+            let num = Math.floor(Math.random() * fortuneCookie.length)
+            if(!randNumArray.includes(num)) {
+                randNumArray.push(num)
+                cookieCollection.push({'cookie': fortuneCookie[num]})
+            }
+        }
+        return cookieCollection
+    }
+}
+
+//Middlewares
+app.use(morgan('combined'))
+
+//app.use(cors())
+
+app.use(express.static(__dirname + '/frontend'))
+
+app.get('/', (req, resp) => {
+    resp.status(200)
+    resp.type('text/html')
+    resp.send('Homepage')
+})
+
+app.get('/api/cookie', cors(), (req, resp) => {
+    let count = req.query['count']
+    let cookie = []
+    if(count == undefined) {
+        cookie = getCookies()
+    }
+    else {
+        count = parseInt(count)
+        cookie = getCookies(count)
+    }
+    
+    resp.status(200)
+    resp.type('application/json')
+    resp.json(cookie)
+})
+
+//Start the app
+app.listen(PORT, () => {
+    console.info(`App has started on port ${PORT} at ${new Date()}`)
+})
